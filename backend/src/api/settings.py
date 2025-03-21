bc2c8136fadddc4bbb6ba2d44f492ee14c31c905
Yue <yue.su@dal.ca>
2025-03-21 02:28:15 +0000

    Add Whitenoise for static file management in backend
    - Include Whitenoise middleware in settings.py to serve static files efficiently.
    - Update requirements.txt to include Whitenoise version 6.6.0.
    - Set STATIC_ROOT and configure STATICFILES_STORAGE for optimized static file handling.

--- backend/src/api/settings.py
+++ backend/src/api/settings.py
@@ -99,6 +99,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -226,6 +227,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
